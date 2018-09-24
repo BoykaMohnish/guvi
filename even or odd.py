c2549c7cccb4b68a8252e7e2d7c8710b67887579
BoykaMohnish <42800395+BoykaMohnish@users.noreply.github.com>
2018-09-24 12:20:14 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,7 +1,7 @@
 a = int(input("Enter a number: "))
-if (a%9==0):
+if (a%2==0):
    print("{0} is Even".format(a))
-elif(a%5==0):
+elif(a%3==0):
    print("{0} is Odd".format(a))
 else:
    print("invalid")
